536c1dab945bf0c8bc0bb99563d966727b668fad
Daniel Armstrong <33160370+armstrongdj@users.noreply.github.com>
2020-09-27 17:51:41 -0700

    Add swagger

--- ContactsAPI/ContactsAPI.Web/Startup.cs
+++ ContactsAPI/ContactsAPI.Web/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace ContactsAPI.Web
 {
@@ -26,11 +27,33 @@ namespace ContactsAPI.Web
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "Contacts API",
+                    Description = "An API for managing employee contacts",
+                    Contact = new OpenApiContact
+                    {
+                        Name = "Daniel Armstrong",
+                        Email = string.Empty,
+                        Url = new Uri("https://github.com/armstrongdj/demo-contact-api")
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Contacts API V1");
+            });
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
